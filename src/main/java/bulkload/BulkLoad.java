c2f1d69d060410ae62da367eac78dac88f878763
Joe Olson <jolson7168@nododos.com>
2016-11-21 08:16:51 -0600

    updated

--- src/main/java/bulkload/BulkLoad.java
+++ src/main/java/bulkload/BulkLoad.java
@@ -38,6 +38,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
